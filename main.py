c6d5696fe21d366f1a0fe9dd7d0995a50e6b95b2
Suryanarayanan Venkatesan <suryan.venkat@gmail.com>
2025-03-19 22:40:42 +0100

    retain bloglist only

--- main.py
+++ main.py
@@ -5,10 +5,15 @@ def clone_github_repo(repo_url):
     try:
         os.system(f"git clone {repo_url}")
         print(f"Repository cloned successfully: {repo_url}")
+        # only interested in part4 bloglist so moving that to root
+
+        os.makedirs("bloglist", exist_ok=True)
+        os.system("cp -r fullstackopen/part4/bloglist/bloglist-backend/* bloglist")
+        os.system("rm -r fullstackopen")
     except Exception as e:
         print(f"Failed to clone the repository. Error: {e}")
 
 
 if __name__ == "__main__":
-    if not os.path.exists('fullstackopen'):
+    if not os.path.exists('bloglist'):
         clone_github_repo('https://github.com/suryanarayanan-v/fullstackopen.git')
